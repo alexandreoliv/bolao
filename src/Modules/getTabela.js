9e891bfd84c2a5a757e07e4eb1e3121cf9172bf6
alexandreoliv <alexandrebo@gmail.com>
2022-04-10 01:57:50 +0200

    refactor getTabela

--- src/Modules/getTabela.js
+++ src/Modules/getTabela.js
@@ -3,38 +3,39 @@ const API_KEY = process.env.REACT_APP_SECRET_KEY;
 
 export const getTabela = (serie) => {
 	// console.info("inside getTabela()");
-	// Local method:
-	const tabela = require("../Data/tabela.json");
-	// console.info("tabela: ", tabela);
 
-	const sortedTabela = tabela
-		.map((e) => ({
-			nome: e.time.nome_popular,
-			posicao: e.posicao,
-		}))
-		.sort((a, b) => {
-			return a.nome.toUpperCase() < b.nome.toUpperCase() ? -1 : 1;
-		});
-	// console.info("sortedTabela: ", sortedTabela);
-
-	return sortedTabela;
+	const sortedTabela = (tabela) => {
+		return tabela
+			.map((e) => ({
+				nome: e.time.nome_popular,
+				posicao: e.posicao,
+			}))
+			.sort((a, b) => {
+				return a.nome.toUpperCase() < b.nome.toUpperCase() ? -1 : 1;
+			});
+	};
 
 	// // API method:
-	// axios
+	// return axios
 	// 	.get("https://api.api-futebol.com.br/v1/campeonatos/10/tabela/", {
 	// 		headers: {
 	// 			Authorization: `Bearer ${API_KEY}`,
 	// 		},
 	// 	})
 	// 	.then(function (response) {
-	// 		console.info(response.data);
+	// 		// console.info("Tabela from API: ", response.data);
 	// 		return response.data;
 	// 	})
 	// 	.catch(function (error) {
-	// 		// handle error
 	// 		console.info(error);
 	// 	})
-	// 	.then(function () {
+	// 	.then(function (tabela) {
 	// 		// always executed
+	// 		return sortedTabela(tabela);
 	// 	});
+
+	// Local method:
+	const tabela = require("../Data/tabela.json");
+	// console.info("tabela: ", tabela);
+	return sortedTabela(tabela);
 };
