62cdb6c6a9a83836631492362d6e9a810b14771a
alexandreoliv <alexandrebo@gmail.com>
2022-07-19 16:09:40 +0200

    transform all other components into functional

--- src/Components/Classificacao.js
+++ src/Components/Classificacao.js
@@ -1,40 +1,39 @@
 import { Table } from "antd";
-import { Component } from "react";
 
-export default class Classificacao extends Component {
-	render() {
-		const {
-			serie,
-			classificacaoColumnsA,
-			classificacaoDataA,
-			classificacaoColumnsB,
-			classificacaoDataB,
-		} = this.props;
-		return (
-			<div>
-				<h2
-					style={{
-						fontWeight: "bold",
-						margin: "0 0 10px 10px",
-						textAlign: "center",
-					}}
-				>
-					Classificação Série {serie}
-				</h2>
-				<Table
-					className="small-table"
-					columns={
-						serie === "A"
-							? classificacaoColumnsA
-							: classificacaoColumnsB
-					}
-					dataSource={
-						serie === "A" ? classificacaoDataA : classificacaoDataB
-					}
-					pagination={false}
-					size={"small"}
-				/>
-			</div>
-		);
-	}
-}
+const Classificacao = (props) => {
+	const {
+		serie,
+		classificacaoColumnsA,
+		classificacaoDataA,
+		classificacaoColumnsB,
+		classificacaoDataB,
+	} = props;
+	return (
+		<div>
+			<h2
+				style={{
+					fontWeight: "bold",
+					margin: "0 0 10px 10px",
+					textAlign: "center",
+				}}
+			>
+				Classificação Série {serie}
+			</h2>
+			<Table
+				className="small-table"
+				columns={
+					serie === "A"
+						? classificacaoColumnsA
+						: classificacaoColumnsB
+				}
+				dataSource={
+					serie === "A" ? classificacaoDataA : classificacaoDataB
+				}
+				pagination={false}
+				size={"small"}
+			/>
+		</div>
+	);
+};
+
+export default Classificacao;
--- src/Components/Distancia.js
+++ src/Components/Distancia.js
@@ -1,36 +1,33 @@
 import { Table } from "antd";
-import { Component } from "react";
 
-export default class Distancia extends Component {
-	render() {
-		const {
-			serie,
-			distanciaColumnsA,
-			distanciaDataA,
-			distanciaColumnsB,
-			distanciaDataB,
-		} = this.props;
-		return (
-			<div>
-				<h2
-					style={{
-						fontWeight: "bold",
-						margin: "0 0 10px 10px",
-						textAlign: "center",
-					}}
-				>
-					Distância Para o Acerto Série {serie}
-				</h2>
-				<Table
-					className="big-table"
-					columns={
-						serie === "A" ? distanciaColumnsA : distanciaColumnsB
-					}
-					dataSource={serie === "A" ? distanciaDataA : distanciaDataB}
-					pagination={false}
-					size={"small"}
-				/>
-			</div>
-		);
-	}
-}
+const Distancia = (props) => {
+	const {
+		serie,
+		distanciaColumnsA,
+		distanciaDataA,
+		distanciaColumnsB,
+		distanciaDataB,
+	} = props;
+	return (
+		<div>
+			<h2
+				style={{
+					fontWeight: "bold",
+					margin: "0 0 10px 10px",
+					textAlign: "center",
+				}}
+			>
+				Distância Para o Acerto Série {serie}
+			</h2>
+			<Table
+				className="big-table"
+				columns={serie === "A" ? distanciaColumnsA : distanciaColumnsB}
+				dataSource={serie === "A" ? distanciaDataA : distanciaDataB}
+				pagination={false}
+				size={"small"}
+			/>
+		</div>
+	);
+};
+
+export default Distancia;
