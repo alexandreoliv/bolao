997b29f6ff10b7dfcaf7add25ddddfadf332b938
alexandreoliv <alexandrebo@gmail.com>
2022-07-19 21:08:19 +0200

    refactor getClassificacao

--- src/modules/getClassificacao.js
+++ src/modules/getClassificacao.js
@@ -1,62 +1,12 @@
 export const getClassificacao = (keys, apostasData) => {
 	const pontuacaoDetalhada = getPontuacaoDetalhada(keys, apostasData);
-	console.log("pontuacaoDetalhada", pontuacaoDetalhada);
-
-	const pontuacaoFinal = pontuacaoDetalhada.map((p) => {
-		return p.reduce((prev, cur) => prev + cur, 0);
-	});
-
+	const pontuacaoFinal = getPontuacaoFinal(pontuacaoDetalhada);
 	const filteredKeys = keys.filter((k) => k !== "Equipe" && k !== "Atual");
-
-	const classificacaoData = [];
-	console.log("pontuacaoArray", pontuacaoFinal);
-	for (let i = 0; i < filteredKeys.length; i++) {
-		classificacaoData[i] = {};
-		classificacaoData[i]["nome"] = filteredKeys[i];
-		classificacaoData[i]["pontuacao"] = pontuacaoFinal[i];
-		classificacaoData[i]["key"] = filteredKeys[i];
-	}
-	console.log("classificacaoData", classificacaoData);
-
-	classificacaoData.sort(function (a, b) {
-		// mutates classificacaoData
-		return b.pontuacao - a.pontuacao;
-	});
-
-	// mutates classificacaoData to add each bettor's current position
-	classificacaoData[0].posicao = 1;
-	for (let i = 1; i < classificacaoData.length; i++) {
-		if (
-			classificacaoData[i].pontuacao ===
-			classificacaoData[i - 1].pontuacao
-		)
-			classificacaoData[i].posicao = classificacaoData[i - 1].posicao;
-		else classificacaoData[i].posicao = i + 1;
-	}
-
-	const classificacaoColumns = [
-		{
-			title: "Posição",
-			key: "posicao",
-			dataIndex: "posicao",
-			align: "center",
-			width: "15%",
-		},
-		{
-			title: "Nome",
-			key: "nome",
-			dataIndex: "nome",
-			align: "center",
-			width: "30%",
-		},
-		{
-			title: "Pontuação",
-			key: "pontuacao",
-			dataIndex: "pontuacao",
-			align: "center",
-			width: "15%",
-		},
-	];
+	const classificacaoData = getClassificacaoData(
+		filteredKeys,
+		pontuacaoFinal
+	);
+	const classificacaoColumns = getClassificacaoColumns();
 
 	return {
 		classificacaoColumns,
@@ -86,4 +36,68 @@ const getPontuacaoDetalhada = (keys, apostasData) => {
 		.map((k) => {
 			return geraArray(k, apostasData);
 		});
+};
+
+const getPontuacaoFinal = (pontuacaoDetalhada) => {
+	return pontuacaoDetalhada.map((p) =>
+		p.reduce((prev, cur) => prev + cur, 0)
+	);
+};
+
+const getClassificacaoData = (filteredKeys, pontuacaoFinal) => {
+	let classificacaoData = [];
+	for (let i = 0; i < filteredKeys.length; i++) {
+		classificacaoData[i] = {};
+		classificacaoData[i]["nome"] = filteredKeys[i];
+		classificacaoData[i]["pontuacao"] = pontuacaoFinal[i];
+		classificacaoData[i]["key"] = filteredKeys[i];
+	}
+	classificacaoData.sort(function (a, b) {
+		// mutates classificacaoData
+		return b.pontuacao - a.pontuacao;
+	});
+
+	classificacaoData = addPosicaoApostador(classificacaoData);
+	return classificacaoData;
+};
+
+const addPosicaoApostador = (classificacaoData) => {
+	let classificacaoDataClone = JSON.parse(JSON.stringify(classificacaoData));
+	classificacaoDataClone[0].posicao = 1;
+	for (let i = 1; i < classificacaoDataClone.length; i++) {
+		if (
+			classificacaoDataClone[i].pontuacao ===
+			classificacaoDataClone[i - 1].pontuacao
+		)
+			classificacaoDataClone[i].posicao =
+				classificacaoDataClone[i - 1].posicao;
+		else classificacaoDataClone[i].posicao = i + 1;
+	}
+	return classificacaoDataClone;
+};
+
+const getClassificacaoColumns = () => {
+	return [
+		{
+			title: "Posição",
+			key: "posicao",
+			dataIndex: "posicao",
+			align: "center",
+			width: "15%",
+		},
+		{
+			title: "Nome",
+			key: "nome",
+			dataIndex: "nome",
+			align: "center",
+			width: "30%",
+		},
+		{
+			title: "Pontuação",
+			key: "pontuacao",
+			dataIndex: "pontuacao",
+			align: "center",
+			width: "15%",
+		},
+	];
 };
