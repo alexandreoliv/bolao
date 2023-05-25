be084e6a8ee978b641171330df65567b889158e2
alexandreoliv <alexandrebo@gmail.com>
2023-05-25 21:01:54 +0200

    update getTabela to retrieve Série A data from the API

--- frontend/src/modules/getTabela.js
+++ frontend/src/modules/getTabela.js
@@ -3,29 +3,29 @@ const API_KEY = process.env.REACT_APP_SECRET_KEY;
 
 export const getTabela = async (ano, serie) => {
 	// API method:
-	// if (ano === 2023 && (serie === "A" || serie === "B")) {
-	// 	// offline way:
-	// 	let tabela = require("../data/tabela2023A.json");
+	if (ano === 2023 && serie === "A") {
+		// 	// offline way:
+		// 	let tabela = require("../data/tabela2023A.json");
 
-	// 	// online way:
-	// 	// let tabela = await axios
-	// 	// 	.get("https://api.api-futebol.com.br/v1/campeonatos/10/tabela/", {
-	// 	// 		headers: {
-	// 	// 			Authorization: `Bearer ${API_KEY}`,
-	// 	// 		},
-	// 	// 	})
-	// 	// 	.then((response) => response.data)
-	// 	// 	.catch((error) => console.log(error));
+		// online way:
+		let tabela = await axios
+			.get("https://api.api-futebol.com.br/v1/campeonatos/10/tabela/", {
+				headers: {
+					Authorization: `Bearer ${API_KEY}`,
+				},
+			})
+			.then((response) => response.data)
+			.catch((error) => console.log(error));
 
-	// 	// for both ways:
-	// 	tabela.sort((a, b) =>
-	// 		a.time.nome_popular.localeCompare(b.time.nome_popular)
-	// 	);
-	// 	const equipes = tabela.map((t) => t.time.nome_popular);
-	// 	const posicoes = tabela.map((t) => t.posicao);
-	// 	tabela = { ano, serie, equipes, posicoes };
-	// 	return tabela;
-	// }
+		// for both ways:
+		tabela.sort((a, b) =>
+			a.time.nome_popular.localeCompare(b.time.nome_popular)
+		);
+		const equipes = tabela.map((t) => t.time.nome_popular);
+		const posicoes = tabela.map((t) => t.posicao);
+		tabela = { ano, serie, equipes, posicoes };
+		return tabela;
+	}
 
 	return await axios
 		.get(
